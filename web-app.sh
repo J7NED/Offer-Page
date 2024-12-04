7167131d56a58f4e6f3ef772d18b23e314f062b0
Ubuntu <ubuntu@ip-172-31-30-216.ec2.internal>
2024-12-04 14:29:35 +0000

    hello JD offer

--- web-app.sh
+++ web-app.sh
@@ -17,7 +17,7 @@ echo "EXPOSE 5000" >> tempdir/Dockerfile
 echo "CMD python /webapp/app.py" >> tempdir/Dockerfile
 
 
-cd tempdir 
-sudo docker build -t webapp .
-sudo docker run -t -d -p 5000:5000 --name samplerun webapp 
-sudo docker ps -a
+cd tempdir
+docker build -t webapp .
+docker run -t -d -p 5000:5000 --name samplerun webapp 
+docker ps -a
