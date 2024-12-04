e22f87e98292821e435bef14e5e70ff5884d5161
Ubuntu <ubuntu@ip-172-31-30-216.ec2.internal>
2024-12-04 13:24:47 +0000

    hi people

--- web-app.sh
+++ web-app.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-mkdir tempdir
-mkdir tempdir/templates
-mkdir tempdir/static
+mkdir -p tempdir
+mkdir -p tempdir/templates
+mkdir -p tempdir/static
 
-cp _app.py tempdir/.
+cp app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
@@ -14,9 +14,10 @@ echo "COPY ./static /webapp/static/" >> tempdir/Dockerfile
 echo "COPY ./templates /webapp/templates/" >> tempdir/Dockerfile
 echo "COPY app.py /webapp/" >> tempdir/Dockerfile
 echo "EXPOSE 5000" >> tempdir/Dockerfile
-echo "CMD python /webapp.app.py" >> tempdir/Dockerfile
+echo "CMD python /webapp/app.py" >> tempdir/Dockerfile
+
 
 cd tempdir 
-sudo docker build -t webapp .
-sudo docker run -t -d -p 5000:5000 --name samplerunning webapp 
-sudo docker ps -a
+docker build -t webapp .
+docker run -t -d -p 5000:5000 --name samplerun webapp 
+docker ps -a
